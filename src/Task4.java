e3b26889ab2f876b052f9fd8e308e99ae3bdeddf
MEG-89 <Kitty_M_2005@mail.ru>
2020-06-12 01:09:50 +0400

    Задача №4: Отработка поразрядных операций

---
+++ src/Task4.java
@@ -0,0 +1,24 @@
+import java.awt.image.Raster;
+import java.util.Scanner;
+
+class Task4 {
+    public static void main(String args[]) {
+        System.out.println("Введите любое число в бинарном формате: ");
+        String text = new Scanner(System.in).next();
+        int result = 0;
+        for (int i = 0; i < text.length(); i++)
+            if (text.charAt(text.length() - 1 - i) == '1')
+                result = result + raisingToPower(i, 2);
+
+        System.out.println("Результат перевода Вашего числа в десятичный формат: " + result);
+    }
+
+    private static int raisingToPower(int power, int base) {
+        int result = 1;
+        for (int i = 1; i <= power; i++)
+            result = result * base;
+
+        return result;
+    }
+}
+
