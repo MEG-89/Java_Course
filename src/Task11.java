57922974da65588eec527fb7ff980f703dc838cf
MEG-89 <Kitty_M_2005@mail.ru>
2020-06-16 19:22:28 +0400

    Задача №11

---
+++ src/Task11.java
@@ -0,0 +1,9 @@
+import java.util.Scanner;
+
+public class Task11 {
+    public static void main(String[] args) {
+        System.out.println("Введите текст:");
+        String text = new Scanner(System.in).nextLine();
+        System.out.println(text);
+    }
+}
