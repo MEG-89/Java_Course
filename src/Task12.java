a6e3d6f74515e7e98ee0216fbf0e952933def9a6
MEG-89 <Kitty_M_2005@mail.ru>
2020-06-16 19:28:43 +0400

    Задача №12

---
+++ src/Task12.java
@@ -0,0 +1,8 @@
+import java.util.Scanner;
+
+public class Task12 {
+    public static void main(String[] args) {
+        System.out.println("Введите текст:");
+        System.out.println(new Scanner(System.in).nextLine().replace(" ", ""));
+    }
+}
